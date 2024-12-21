893d63c64987295251a142b84d73d97d745d1dd3
Tadeu <tadeubranconeto@gmail.com>
2024-12-21 17:09:19 -0300

    feat: create post and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Header } from "./components/Header";
-import "./globals.css";
-import styles from "./App.module.css";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./globals.css";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+         <Post />
          <Post />
         </main>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,52 @@
+import styles from "./Post.module.css";
+
 export function Post() {
-    return()
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/tadeubneto.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Tadeu</strong>
+            <span>Developer</span>
+          </div>
+        </div>
+
+        <time title="" dateTime="">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        
+        <p>Acabei de subir mais um projeto no meu portifa. É um
+        projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é
+        DoctorCare 🚀</p>
+        
+        <p><a href="">👉 jane.design/doctorcare</a></p>
+        
+        <p>
+            <a href="">#novoprojeto</a>{' '}
+            <a href="">#nlw</a>{' '}
+            <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+        placeholder="Deixe um comentário"
+        />
+        <footer>
+        <button type="submit">Comentar</button>
+        </footer>
+
+      </form>
+    </article>
+ 
+  );
 }
+
